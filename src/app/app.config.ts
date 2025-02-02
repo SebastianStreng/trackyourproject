decba4ab0822ad597ecdb793102daf6a0ee52288
Sebastian Streng <51957686+SebastianStreng@users.noreply.github.com>
2025-02-02 13:11:15 +0100

    providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,12 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
+    provideRouter([]),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideAnimationsAsync(),
--- src/app/pages/login-page/login.component.ts
+++ src/app/pages/login-page/login.component.ts
@@ -1,17 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DialogService } from 'primeng/dynamicdialog';
+import { Router } from '@angular/router';
 import { SmartphoneCardComponent } from 'src/app/shared/smartphone-card/smartphone-card.component';
+import { AuthenticationService } from 'src/app/core/services/auth-service/authentification-service';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [CommonModule, SmartphoneCardComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  username = '';
+  password = '';
+  error = '';
 
-  constructor () {
+  constructor (private authService: AuthenticationService, private router: Router) {
     
+  }
+
+  signIn() {
+    this.authService.login(this.username, this.password).subscribe(
+      () => {
+        this.router.navigate(['Customize']);
+      },
+      (error) => {
+        this.error = 'Invalid username or password';
+      }
+    );
   }
 }
